ca3f9b7e3b4a42c39c3a402cb209050bceb43036
TAAODMA2 <marco.odermatt@swisscom.com>
2024-11-06 08:52:47 +0100

    first commit

---
+++ src/main/java/ch/m321/m321_notenerfassung/M321NotenerfassungApplication.java
@@ -0,0 +1,13 @@
+package ch.m321.m321_notenerfassung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M321NotenerfassungApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(M321NotenerfassungApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ch/m321/m321_notenerfassung/M321NotenerfassungApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.m321.m321_notenerfassung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M321NotenerfassungApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
